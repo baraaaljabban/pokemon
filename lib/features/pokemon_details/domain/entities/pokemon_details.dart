74f5e26b64072db8ba9bbfca108acd96fb3a87c8
Baraa Aljabban <baraa.aljabban.it@gmail.com>
2023-06-07 11:43:06 +0800

    create model and entity

--- lib/features/pokemon_details/domain/entities/pokemon_details.dart
+++ lib/features/pokemon_details/domain/entities/pokemon_details.dart
@@ -1,1 +1,21 @@
-class PokemonDetails {}
+class PokemonDetails {
+  final String move, dexName, generationName, machineName, typeName, versionName, abilityName, berryName, natureName, locationName;
+  final int rarity, typeSlot, berryGiftPower, experience, experienceLevel;
+  PokemonDetails({
+    required this.move,
+    required this.generationName,
+    required this.machineName,
+    required this.typeName,
+    required this.versionName,
+    required this.abilityName,
+    required this.berryName,
+    required this.natureName,
+    required this.locationName,
+    required this.rarity,
+    required this.typeSlot,
+    required this.berryGiftPower,
+    required this.experience,
+    required this.experienceLevel,
+    required this.dexName,
+  });
+}
