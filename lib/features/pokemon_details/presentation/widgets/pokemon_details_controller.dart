b68a0a06fea6c5231c0e98eaff0defb3d079b432
Baraa Aljabban <baraa.aljabban.it@gmail.com>
2023-06-07 12:49:15 +0800

    working on deatiling UI

--- lib/features/pokemon_details/presentation/widgets/pokemon_details_controller.dart
+++ lib/features/pokemon_details/presentation/widgets/pokemon_details_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pokemon/core/theme/app_style.dart';
 import 'package:pokemon/features/pokemon_details/domain/entities/pokemon_details.dart';
 import 'package:pokemon/features/pokemon_list/presentation/widgets/image_controller.dart';
 
@@ -13,13 +14,69 @@ class PokemonDetailsController extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          height: 300,
-          child: ImageController(imageURL: id),
-        )
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Center(
+            child: SizedBox(
+              height: 300,
+              child: ImageController(imageURL: id),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Generation: ${pokemonDetails.generationName}",
+              style: AppStyle.h5,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Ability: ${pokemonDetails.abilityName}",
+              style: AppStyle.h5,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Gift Power: ${pokemonDetails.berryGiftPower}",
+              style: AppStyle.h5,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Dex Name: ${pokemonDetails.dexName}",
+              style: AppStyle.h5,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Type: ${pokemonDetails.typeName}",
+              style: AppStyle.h5,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Rarity: ${pokemonDetails.rarity}",
+              style: AppStyle.h5,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Slot: ${pokemonDetails.typeSlot}",
+              style: AppStyle.h5,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
