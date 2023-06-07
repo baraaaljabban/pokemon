163f1a633f1a43e9af7a56254e93e44eab2db8d4
Baraa Aljabban <baraa.aljabban.it@gmail.com>
2023-06-07 20:28:02 +0800

    Fixing issue about not reflecting directly and making reuse of exsisting  code

--- lib/features/pokemon_list/presentation/pages/favorite_page.dart
+++ lib/features/pokemon_list/presentation/pages/favorite_page.dart
@@ -26,7 +26,7 @@ class _FavoritePageState extends State<FavoritePage> {
           return BlocProvider.of<PokemonListCubit>(context).favorite;
         },
         builder: (context, state) {
-          return const PokemonsListViewController(showFavIcon: false);
+          return PokemonsListViewController(pokemons: state, showFavIcon: false);
         },
       ),
     );
